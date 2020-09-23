0683feb5362d7a49673fb7bea4aa67917904f6c4
Travis Lee <travisslee223@gmail.com>
2020-09-23 01:09:41 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,12 @@
 class Author
   attr_accessor :name, :post
   
+  @@post_count = 0 
+  
+   def initialize(name)
+    @name = name
+    @posts = []
+  end
   
   def posts 
     Post.all.select {|post| post.author == self}
