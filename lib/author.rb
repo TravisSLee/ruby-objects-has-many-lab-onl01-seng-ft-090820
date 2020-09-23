394c6293f1203e22cbaedeef0040476f9c845295
Travis Lee <travisslee223@gmail.com>
2020-09-23 01:28:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,6 @@
 class Author
   attr_accessor :name, :post
   
-  @@post_count = 0 
   
    def initialize(name)
     @name = name
@@ -15,14 +14,12 @@ class Author
   def add_post(post)
      @posts << post
     post.author = self
-    @@post_count += 1
   end
   
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
-    @@post_count += 1
   end
   
   def self.post_count
